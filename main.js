845b74aa347d88f57358a48e4f66f29a8d22ab8a
Jisie DM <jisie.david@gmail.com>
2018-04-03 13:46:03 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function lasteNameResponder(){
+  const lastName = "Davidsdas";
+  if (lastName.length >= 7) {
+    console.log("I can't pronounce that last name");
+    
+  } else {
+    console.log("The last name is ", lastName); 
+  }   
+
+}
+
+lasteNameResponder();
